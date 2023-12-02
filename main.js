7c539a6121bcb09f7fc90bbd76720b5145b50629
Ayman-M-Ali <ayman.mohmd.ali@gmail.com>
2023-12-02 16:37:10 +0300

    Added Intersection Observer API

---
+++ main.js
@@ -0,0 +1,19 @@
+const sectionElements = document.querySelectorAll('section');
+
+const observeElements = (sectionElements) => {
+  const options = { threshold: .3 };
+
+  const callback = (entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("in-view");
+      } else {
+        entry.target.classList.remove("in-view");
+      }
+    })
+  }
+  const observer = new IntersectionObserver(callback, options);
+  sectionElements.forEach(element => observer.observe(element));
+}
+
+observeElements(sectionElements)
